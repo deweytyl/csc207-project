4c7e079eb4dcd7f68734f82ef05201a74da7fd2e
Tyler Dewey <ty.dewey@gmail.com>
2014-04-15 15:32:06 -0500

    parsePair initial framework
    Added complete parsePair method and static class JSONPair, more documentation to come

--- src/edu/grinnell/tranchri/cohnhan/deweytyl/hardtmad/JSONUtils.java
+++ src/edu/grinnell/tranchri/cohnhan/deweytyl/hardtmad/JSONUtils.java
@@ -7,6 +7,15 @@ import java.util.ArrayList;
  * 
  * Maybe we could make a JSONValueParser object. Up to us. We should talk about
  * it later, but for now let's just write static methods.
+ * 
+ * Character type keys:
+ * String:   's'
+ * Number:   'n'
+ * Object:   'o'
+ * Array:    'a'
+ * Constant: 'c'
+ * 
+ * JSONValue interface might be useful
  */
 public class JSONUtils
 {
@@ -16,6 +25,12 @@ public class JSONUtils
     // TODO Method Stub
     return null;
   } // parseValue(String)
+  
+  public static JSONPair parsePair(String pairStr)
+  {
+    String[] strs = pairStr.split(":");
+    return new JSONPair(strs[0], parseValue(strs[1]));
+  } // parsePair(String pairStr)
   
   public static ArrayList<Object> parseArray(String arrStr)
   {
@@ -29,4 +44,22 @@ public class JSONUtils
     return array;
   } // parseArray(String)
   
+  static class JSONPair 
+  {
+    /**
+     * A String representing the key of this key-value pair
+     */
+    String key;
+    
+    /**
+     * The value of this key-value pair
+     */
+    Object value;
+    
+    public JSONPair(String str, Object o)
+    {
+      key = str;
+      value = o;
+    }
+  }
 } // class JSONUtils
