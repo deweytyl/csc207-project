5d6f2c6d752482b2073fcd58ccb26cdf120c9e70
hd313 <cohnhann@grinnell.edu>
2014-04-15 21:56:04 -0500

    Adding type method

--- src/edu/grinnell/tranchri/cohnhan/deweytyl/hardtmad/JSONParse.java
+++ src/edu/grinnell/tranchri/cohnhan/deweytyl/hardtmad/JSONParse.java
@@ -5,5 +5,43 @@ package edu.grinnell.tranchri.cohnhan.deweytyl.hardtmad;
  */
 public class JSONParse
 {
+  public char type (String str)
+  {
+    char objectType = 0;
+    char first = str.charAt(0); 
+    switch(first) {
+      case '"' :
+        objectType = 's';
+        break;
+      case '{':
+        objectType = 'o';
+        break;
+      case '[':
+        objectType = 'a';
+        break;
+      case 't':
+      case 'f':
+      case 'n':
+        objectType = 'c';
+        break; 
+      case '-':
+      case '0':
+      case '1':
+      case '2':
+      case '4':
+      case '5':
+      case '6':
+      case '7':  
+      case '8':
+      case '9':
+        objectType = 'n'; 
+        break;
+        
+        
+    }
+    return objectType; 
+  }
+
+  public Object value; 
   
 }
--- src/edu/grinnell/tranchri/cohnhan/deweytyl/hardtmad/JSONValue.java
+++ src/edu/grinnell/tranchri/cohnhan/deweytyl/hardtmad/JSONValue.java
@@ -1,5 +1,6 @@
 package edu.grinnell.tranchri.cohnhan.deweytyl.hardtmad;
 
+import org.json.simple.JSONArray;  
 /**
  * An interface specifying a class which wraps a JSON type and Java object
  * representing a JSON value
@@ -11,7 +12,46 @@ public interface JSONValue
    * JSONValue object based on this table: String: 's' Number: 'n' Object: 'o'
    * Array: 'a' Constant: 'c'
    */
-  public char type();
+
+  /**
+   * When looking at the JSONValue If the first character is: 
+   * { - it is an object 
+   * [ - it is an array 
+   * " - it is a string 
+   * t - it is a constant 
+   * f - it is a constant 
+   * n - it is a constant 
+   * - - it is a number 
+   * (any number 0-9) - it is a number
+   */
+  public char type (String str)
+  {
+    char objectType;
+    char first = str.charAt(0); 
+    switch(first) {
+      case '"' :
+        objectType = 's';
+        break;
+      case '{':
+        objectType = 'o';
+        break;
+      case '-':
+      case '0':
+      case '1':
+      case '2':
+      case '4':
+      case '5':
+      case '6':
+      case '7':  
+      case '8':
+      case '9':
+        objectType = 'n'; 
+        break;
+        
+    }
+    
+    return objectType;
+  }
 
   /**
    * Get the wrapped object
@@ -19,4 +59,5 @@ public interface JSONValue
    * @return obj the Java representation of the JSON value
    */
   public Object value();
+
 }
