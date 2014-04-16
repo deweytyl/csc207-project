04023fb2b910f9801891c7cc59137a23599f81ea
Christine Tran <tranchri@grinnell.edu>
2014-04-15 21:06:52 -0500

    Moved parseNumber and parseE into JSONNumber

--- src/edu/grinnell/tranchri/cohnhan/deweytyl/hardtmad/JSONNumber.java
+++ src/edu/grinnell/tranchri/cohnhan/deweytyl/hardtmad/JSONNumber.java
@@ -25,5 +25,46 @@ public class JSONNumber
   {
     return this.value;
   } // value()
+  
+  /**
+   * Given a JSON string return a BigDecimal object.
+   * @param str
+   * @return BigDecimal
+   * @throws Exception when str is not correct JSON syntax
+   */
+  public BigDecimal parseNumber(String str)
+    throws Exception
+  {
+    if (str.contains("e"))
+      {
+        this.value = parseE(str);
+      } // if exp or frac exp
+
+    // The number is an int or frac (double)
+    this.value = new BigDecimal(str);
+    return this.value;
+  } // parseNumber(String)
+
+  /**
+   * Parse str around 'e' and find the value of #e# and return
+   * BigDecimal object with that value.
+   * @param str
+   * @return BigDecimal
+   * @throws Exception if incorrect JSON syntax
+   */
+  public static BigDecimal parseE(String str)
+    throws Exception
+  {
+    String nums[] = str.split("e");
+    if (nums.length != 2)
+      {
+        throw new Exception("Incorrect JSON syntax");
+      } // if
+    else
+      {
+        return new BigDecimal(Math.pow(Double.parseDouble(nums[0]),
+                                       Double.parseDouble(nums[1])));
+      } // else
+  } // parseE(String)
 
 } //class JSONNumber
--- src/edu/grinnell/tranchri/cohnhan/deweytyl/hardtmad/JSONUtils.java
+++ src/edu/grinnell/tranchri/cohnhan/deweytyl/hardtmad/JSONUtils.java
@@ -52,45 +52,6 @@ public class JSONUtils
     return null;
   } // getString(JSONValue)
 
-  /**
-   * Given a JSON string return a BigDecimal object.
-   * @param str
-   * @return BigDecimal
-   * @throws Exception when str is not correct JSON syntax
-   */
-  public static BigDecimal parseNumber(String str)
-    throws Exception
-  {
-    if (str.contains("e"))
-      {
-        parseE(str);
-      } // if exp or frac exp
-
-    // The number is an int or frac (double)
-    return new BigDecimal(str);
-  } // parseNumber(String)
-
-  /**
-   * Parse str around 'e' and find the value of #e# and return
-   * BigDecimal object with that value.
-   * @param str
-   * @return BigDecimal
-   * @throws Exception if incorrect JSON syntax
-   */
-  public static BigDecimal parseE(String str)
-    throws Exception
-  {
-    String nums[] = str.split("e");
-    if (nums.length != 2)
-      {
-        throw new Exception("Incorrect JSON syntax");
-      } // if
-    else
-      {
-        return new BigDecimal(Math.pow(Double.parseDouble(nums[0]),
-                                       Double.parseDouble(nums[1])));
-      } // else
-  } // parseE(String)
 
   static class JSONPair
   {
