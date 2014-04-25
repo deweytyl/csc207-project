d1b427ffd6761d048cadb1bea5745cc78a7cbb7c
Christine Tran <tranchri@grinnell.edu>
2014-04-25 17:36:23 -0500

    Fixed bug

--- src/edu/grinnell/tranchri/cohnhan/deweytyl/hardtmad/JSONSymbolicConstant.java
+++ src/edu/grinnell/tranchri/cohnhan/deweytyl/hardtmad/JSONSymbolicConstant.java
@@ -75,10 +75,10 @@ public class JSONSymbolicConstant
   /**
    * Given a JSON string return a JSONSymbolicConstant object.
    * 
-   * @param str
+   * @param charQueue
    * @return JSONSymbolicConstantg
    * @throws Exception
-   *           when str is not correct JSON syntax
+   *           when not correct JSON syntax
    */
   public static JSONSymbolicConstant
     parseSymbolicConstant(Queue<Character> charQueue)
@@ -87,12 +87,51 @@ public class JSONSymbolicConstant
     // Make String
     String val = "";
     
-    // Until there is nothing left to look at
-    while (charQueue.peek() != null)
-      {
-        // Add the character read to val
-        val += charQueue.poll();
-      } // while
+    // Following code taken from Sam Rebelsky's JSON.java
+    // and adapted.
+    // Get the first character
+    char ch = charQueue.poll();
+    // Cases for t,f, and n. Check to make sure that
+    // they are actually true, false, and null, throws
+    // an exception otherwise.
+    switch(ch)
+    {
+      case 't':
+        if((charQueue.poll() == 'r') && (charQueue.poll() == 'u') &&
+            (charQueue.poll() == 'e'))
+          {
+            val = "true";
+          } // if
+        else
+          {
+            throw new Exception ("Invalid constant starting with t.");
+          }
+        break;
+      case 'f':
+        if((charQueue.poll() == 'a') && (charQueue.poll() == 'l') &&
+            (charQueue.poll() == 's') && (charQueue.poll() == 'e'))
+          {
+            val = "false";
+          } // if
+        else
+          {
+            throw new Exception ("Invalid constant starting with f.");
+          }
+        break;
+      case 'n':
+        if((charQueue.poll() == 'u') && (charQueue.poll() == 'l') &&
+            (charQueue.poll() == 'l'))
+          {
+            val = "null";
+          } // if
+        else
+          {
+            throw new Exception ("Invalid constant starting with n.");
+          }
+        break;
+      default:
+        throw new Exception("Invalid constant.");
+    } // switch
 
     return new JSONSymbolicConstant(val);
 
